cb57cbe3cce83aa16de5e05a96d5706cf682dc9a
karol-202 <karoljurski1@gmail.com>
2019-06-21 15:30:54 +0200

    Remove redundant Time functions.

--- client/src/androidMain/java/pl/karol202/sciorder/client/common/extensions/Time.kt
+++
@@ -1,3 +0,0 @@
-package pl.karol202.sciorder.client.common.extensions
-
-actual fun currentTimeMillis() = System.currentTimeMillis()
--- client/src/commonMain/kotlin/pl/karol202/sciorder/client/common/extensions/Time.kt
+++ client/src/commonMain/kotlin/pl/karol202/sciorder/client/common/extensions/Time.kt
@@ -1,13 +1,7 @@
-@file:JvmName("TimeAndroidKt")
-
 package pl.karol202.sciorder.client.common.extensions
-
-import kotlin.jvm.JvmName
 
 val Int.millis get() = toLong()
 val Int.seconds get() = 1000 * millis
 val Int.minutes get() = 60 * seconds
 val Int.hours get() = 60 * minutes
 val Int.days get() = 24 * hours
-
-expect fun currentTimeMillis(): Long
